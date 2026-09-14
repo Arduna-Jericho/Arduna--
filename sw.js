1c21628ea0e964ac3258888f6c54ab2c98e4b218
suad-7 <suad.mughrabi57@gmail.com>
2026-09-14 11:26:52 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-// خدمة بسيطة فقط عشان أندرويد يعتبر الموقع تطبيق حقيقي قابل للتثبيت (بدون أي تخزين مؤقت للمحتوى)
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
